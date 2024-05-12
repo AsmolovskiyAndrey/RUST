3a4bda7cc3e7e043fb84141e83c4b8a638f29715
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2024-05-12 14:28:56 +0200

    check

---
+++ app/target/debug/deps/app-14d25524762f6434.d
@@ -0,0 +1,5 @@
+D:\Developer\Rust_project\RUST\app\target\debug\deps\libapp-14d25524762f6434.rmeta: src\main.rs
+
+D:\Developer\Rust_project\RUST\app\target\debug\deps\app-14d25524762f6434.d: src\main.rs
+
+src\main.rs:
---
+++ app/target/debug/deps/app-27b6dc029a3c3015.d
@@ -0,0 +1,5 @@
+D:\Developer\Rust_project\RUST\app\target\debug\deps\libapp-27b6dc029a3c3015.rmeta: src\main.rs
+
+D:\Developer\Rust_project\RUST\app\target\debug\deps\app-27b6dc029a3c3015.d: src\main.rs
+
+src\main.rs:
