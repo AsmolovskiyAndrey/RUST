cf0d13e3a331d0d6efd691eabd4d896f8a51179d
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2024-05-26 13:05:10 +0200

    ende branch

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,9 +3,12 @@ fn main() {
     println!("My age is {}", age); // wird alles, was in geschweiften Klammern steht, als eine Variable ausgegeben, die ginter dem Komma steht
     println!("{}", age); // nur die "age" drucken (21)
 
-    let mut name: &str = "Andrii";
+    // let mut name: &str = "Andrii"; // ein möglicher, aber unsicherer Weg, eine Zeichenkette zuzuweisen
+    let mut name: String = String::from("Andrii"); // ein gutes Praxis, eine String-Variable zuzuweisen
     println!("{}", name);
-    name = "Andr";
+    name = String::from("ANDR"); // Methode 1: Verwendung der String::from
+    println!("{}", name);
+    name = "And".to_string(); // Methode 2: Verwendung der Methode to_string
     println!("{}", name);
 
 }
